dcf24079376557ef43da3300f72765dea2f9f987
Pavel <pavel@pavel.com>
2021-02-20 17:38:09 +0300

    fix post request

--- db/init_data.sql
+++ db/init_data.sql
@@ -3,3 +3,6 @@ INSERT INTO `microservice` (`id`, `name`) VALUES
 (2, 'orders'),
 (3, 'reports'),
 (4, 'search');
+
+ALTER TABLE `microservice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
